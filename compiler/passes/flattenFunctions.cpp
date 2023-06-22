4ab9ee2a0b45edac6fadfdab8b6f551440bc48b9
Jade Abraham <jade.abraham@hpe.com>
2023-06-22 12:06:07 -0700

    fix for begin task intents
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- compiler/passes/flattenFunctions.cpp
+++ compiler/passes/flattenFunctions.cpp
@@ -283,7 +283,8 @@ addVarsToFormals(FnSymbol* fn, SymbolMap* vars) {
 
       bool isArgThis = sym->hasFlag(FLAG_ARG_THIS);
       bool isCoforall = fn->hasFlag(FLAG_COBEGIN_OR_COFORALL);
-      bool optOut = isArgThis || isCoforall;
+      bool isBegin = fn->hasFlag(FLAG_BEGIN);
+      bool optOut = isArgThis || isCoforall || isBegin;
 
       if (passByRef(sym)) {
         // The task function can take in its argument by REF_MAYBE_CONST
