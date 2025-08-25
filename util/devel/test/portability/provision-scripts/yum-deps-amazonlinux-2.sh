b5c7941334b23740673c32bfa96dbece3237846f
Jade Abraham <jade.abraham@hpe.com>
2025-08-25 12:16:46 -0700

    remove usage of sudo since it can cause problems
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/devel/test/portability/provision-scripts/yum-deps-amazonlinux-2.sh
+++ util/devel/test/portability/provision-scripts/yum-deps-amazonlinux-2.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-alias unsudo="sudo -u bin"
 alias hide=eval
 
 yum -y install git gcc gcc-c++ m4 perl python tcsh bash perl python python-devel python-setuptools bash make gawk python3 which
 
 yum -y install wget tar openssl-devel
-
 hide yum -y install sudo
 
 
-hide MYTMP=`unsudo mktemp -d`
+hide MYTMP=`su - bin -c "mktemp -d"`
 hide cd $MYTMP
-unsudo wget https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1.tar.gz
-unsudo tar xvzf cmake-3.25.1.tar.gz
-cd cmake-3.25.1
-unsudo ./bootstrap
-unsudo make
-make install
 
+su - bin -c "wget https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1.tar.gz"
+su - bin -c "tar xvzf cmake-3.25.1.tar.gz"
+su - bin -c "cd cmake-3.25.1 && ./bootstrap && make"
+make install
 update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1
