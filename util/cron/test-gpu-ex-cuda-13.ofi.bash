4471d1b30a7f3c5c3bfb4a74f8c8751631e24d08
Jade Abraham <jade.abraham@hpe.com>
2026-06-25 14:12:14 -0700

    switch testing to cuda 13
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/cron/test-gpu-ex-cuda-13.ofi.bash
+++ util/cron/test-gpu-ex-cuda-13.ofi.bash
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 #
-# GPU native testing on a Cray EX (using none for CHPL_COMM)
+# GPU native testing on a Cray EX
 
 UTIL_CRON_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) ; pwd)
 source $UTIL_CRON_DIR/common-native-gpu.bash
 source $UTIL_CRON_DIR/common-hpe-cray-ex.bash
-source $UTIL_CRON_DIR/common-gpu-hpe-cray-ex.bash
 source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex.bash
-source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex-cuda-12.bash
+source $UTIL_CRON_DIR/common-gpu-nvidia-hpe-cray-ex-cuda-13.bash
 
-export CHPL_COMM=none
+export CHPL_COMM=ofi
 export CHPL_GPU=nvidia  # amd is also detected automatically
-
-export CHPL_GPU_SPECIALIZATION=y
 
 module list
 
-export CHPL_NIGHTLY_TEST_CONFIG_NAME="gpu-ex-cuda-12.specialization"
+export CHPL_NIGHTLY_TEST_CONFIG_NAME="gpu-ex-cuda-13.ofi"
 $UTIL_CRON_DIR/nightly -cron ${nightly_args}
