cfb2ccb772bf22c233963803b9d3cda927edfbf9
Anna Rift <anna.rift@hpe.com>
2026-07-02 14:20:40 -0700

    See what happens if we don't disable ibv
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/common-gasnet.bash
+++ util/cron/common-gasnet.bash
@@ -12,7 +12,6 @@ export CHPL_COMM=gasnet
 export GASNET_SPAWNFN=L
 export CHPL_RT_MASTERIP=127.0.0.1
 export GASNET_ROUTE_OUTPUT=0
-export CHPL_GASNET_CFG_OPTIONS=--disable-ibv
 
 # Bump the timeout slightly just because we're oversubscribed
 export CHPL_TEST_TIMEOUT=500
