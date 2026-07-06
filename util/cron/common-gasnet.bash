5d07269ed48b6c7f366cf7b1929fceac695c9817
Anna Rift <anna.rift@hpe.com>
2026-07-06 10:40:30 -0700

    Set CHPL_COMM_SUBSTRATE=udp
    Signed-off-by: Anna Rift <anna.rift@hpe.com>

--- util/cron/common-gasnet.bash
+++ util/cron/common-gasnet.bash
@@ -12,6 +12,7 @@ export CHPL_COMM=gasnet
 export GASNET_SPAWNFN=L
 export CHPL_RT_MASTERIP=127.0.0.1
 export GASNET_ROUTE_OUTPUT=0
+CHPL_COMM_SUBSTRATE=udp
 
 # Bump the timeout slightly just because we're oversubscribed
 export CHPL_TEST_TIMEOUT=500
